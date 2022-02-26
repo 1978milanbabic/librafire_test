fd515138b180d2cee68a29670c05a7729dfb0cea
1978milanbabic <1978milan.babic@gmail.com>
2022-02-26 23:42:33 +0100

    [NotFound] page 404 completed

--- src/App/scenes/NotFound/index.js
+++ src/App/scenes/NotFound/index.js
@@ -1,14 +1,16 @@
-
+import { Link } from 'react-router-dom'
 
 // styles
 import './assets/NotFound.scss'
 
-const NotFound = props => {
-  return (
-    <div className='NotFound'>
-      <p>Not Found Page!!!!</p>
+const NotFound = () => (
+  <main className='NotFound'>
+    <div className='centered'>
+      <h1>404! - Page Not Found!!!!</h1>
+      <p>The page you are looking for does not exists!</p>
+      <p>Try with <Link to='/posts'>Posts</Link> page.</p>
     </div>
-  )
-}
+  </main>
+)
 
 export default NotFound
