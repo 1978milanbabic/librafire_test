8c4dd048796778cb902e4affe97740526cfd54bc
1978milanbabic <1978milan.babic@gmail.com>
2022-02-27 16:01:12 +0100

    [Items]{styling} completed

--- src/App/scenes/Posts/components/Items/index.js
+++ src/App/scenes/Posts/components/Items/index.js
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom'
 import './assets/Items.scss'
 
 const Items = ({ currentItems }) => (
-  <>
+  <section className='Items'>
     {currentItems && currentItems.map((item, i) => (
-      <article className='Items' key={i}>
-        <h2>{item.title}</h2>
-        <p>{item.body}</p>
-        <Link to={`/posts/${item.id}`}>Read more -</Link>
+      <article className='Item' key={i}>
+        <div className='Item-inner'>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+          <Link to={`/posts/${item.id}`}>Read More <span>&#8594;</span></Link>
+        </div>
       </article>
     ))}
-  </>
+  </section>
 )
 
 export default Items
