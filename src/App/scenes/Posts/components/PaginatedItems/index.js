19a7d5a6bbf85b9a12b66f5ab8fc0db4442b2f45
1978milanbabic <1978milan.babic@gmail.com>
2022-02-27 13:12:48 +0100

    [PaginatedItems] search by title & filter by author added

--- src/App/scenes/Posts/components/PaginatedItems/index.js
+++ src/App/scenes/Posts/components/PaginatedItems/index.js
@@ -8,8 +8,8 @@ import './assets/PaginatedItems.scss'
 // components
 import Items from '../Items'
 
-const GET_ALL_POSTS = gql`
-  query GetAllPosts (
+const GET_POSTS = gql`
+  query GetPosts (
     $options: PageQueryOptions
   ) {
     posts(options: $options) {
@@ -28,6 +28,35 @@ const GET_ALL_POSTS = gql`
     }
   }
 `
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      data {
+        id
+        name
+      }
+    }
+  }
+`
+const GET_AUTHORS_POSTS = gql`
+  query GetAuthPosts (
+    $id: ID!
+  ) {
+    user (id: $id) {
+      id
+      name
+      posts {
+        data {
+          id
+          title
+          body
+        }
+      }
+    }
+  }
+`
+// prevent overquerying when search
+let responseSearchTimeout
 
 const PaginatedItems = props => {
   // layout constant
@@ -37,19 +66,77 @@ const PaginatedItems = props => {
   const [page, setPage] = useState(1)
   const [count, setCount] = useState(10)
   const [currentItems, setCurrentItems] = useState(null)
+  const [searchTitleInput, setSearchTitleInput] = useState()
+  const [searchTitle, setSearchTitle] = useState()
+  const [authors, setAuthors] = useState()
+  const [author, setAuthor] = useState('')
 
-  // gql lazyQuery
-  const [getAllPosts, { called, loading, data }] = useLazyQuery(GET_ALL_POSTS, {
+  // define get posts lazyQuery
+  const [getPosts, { called, loading, data }] = useLazyQuery(GET_POSTS, {
     fetchPolicy: 'network-only',
     onCompleted: resp => {
-      if (resp && resp.posts && resp.posts && resp.posts.meta) {
+      if (resp && resp.posts && resp.posts.meta) {
         // set data
-        setCurrentItems(resp.posts.data)
-        // set number of posts
-        let nmb = resp.posts.meta.totalCount
-        setCount(Math.ceil(nmb / itemsPerPage))
-        // send number of post to parent component - to set header
-        props.setPostsFound(nmb)
+        let obtainedData = resp.posts.data
+        if (obtainedData.length > 0) {
+          // found something
+          // limit body length
+          let limitedData = obtainedData.map(d => {
+            if (d.body.length > 10) {
+              d.body = d.body.slice(0, 250) + '...'
+            }
+            return d
+          })
+          setCurrentItems(limitedData)
+          // set number of posts
+          let nmb = resp.posts.meta.totalCount
+          setCount(Math.ceil(nmb / itemsPerPage))
+          // send number of post to parent component - to set header
+          props.setPostsFound(nmb)
+        } else {
+          // found nothing
+          setCurrentItems(null)
+          setCount(1)
+          props.setPostsFound(0)
+        }
+      } else {
+        // response incorrect
+        throw new Error('graphQL server bad response!')
+      }
+    },
+    onError: err => {
+      console.log(err)
+      // **** here we can redirect to some graphql error page!!!
+    }
+  })
+  // define get posts by author
+  const [getAuthorPosts, { called: calledAuth, loading: loadingAuth, data: dataAuth }] = useLazyQuery(GET_AUTHORS_POSTS, {
+    fetchPolicy: 'network-only',
+    onCompleted: resp => {
+      if (resp && resp.user && resp.user.posts && resp.user.posts.data) {
+        // set data
+        let obtainedData = resp.user.posts.data
+        if (obtainedData.length > 0) {
+          // found something
+          // limit body length
+          let limitedData = obtainedData.map(d => {
+            if (d.body.length > 10) {
+              d.body = d.body.slice(0, 250) + '...'
+            }
+            return d
+          })
+          setCurrentItems(limitedData)
+          // set number of posts
+          let nmb = resp.user.posts.data.length
+          setCount(Math.ceil(nmb / itemsPerPage))
+          // send number of post to parent component - to set header
+          props.setPostsFound(nmb)
+        } else {
+          // found nothing
+          setCurrentItems(null)
+          setCount(1)
+          props.setPostsFound(0)
+        }
       } else {
         // response incorrect
         throw new Error('graphQL server bad response!')
@@ -61,35 +148,111 @@ const PaginatedItems = props => {
     }
   })
 
-  // Fetch items graphQL on page set (also on initial page)
+  // Fetch items graphQL
   useEffect(() => {
-    getAllPosts({ variables: {
-      options: {
+    if (author === '') {
+      // fetch all / filter by title
+      let options = {
         paginate: {
           page,
           limit: itemsPerPage
         }
       }
-    }})
-  }, [page])
+      if (searchTitle) {
+        options.operators = [
+          {
+            kind: 'LIKE',
+            field: 'title',
+            value: searchTitle
+          }
+        ]
+      }
+      getPosts({ variables: { options }})
+    } else {
+      // fetch by author
+      getAuthorPosts({ variables: { id: parseInt(author) } })
+    }
+  }, [page, searchTitle, author])
 
   // paginate/page change
   const handleChange = (event, value) => {
     setPage(value)
   }
 
+  // search by title
+  const handleSearchByTitle = event => {
+    let { value } = event.target
+    // set input value
+    setSearchTitleInput(value)
+    // reset response timeout
+    clearTimeout(responseSearchTimeout)
+    // do search
+    responseSearchTimeout = setTimeout(() => {
+      // set search start
+      setSearchTitle(value)
+      // reset search by author
+      setAuthor('')
+    }, 1000)
+  }
+
+  // search by user
+  const [getUsers] = useLazyQuery(GET_USERS, { fetchPolicy: 'network-only' })
+  useEffect(() => {
+    // get all users on mount
+    getUsers().then(resp => {
+      if (resp && resp.data && resp.data.users && resp.data.users.data) {
+        let users = resp.data.users.data
+        setAuthors(users)
+      } else {
+        let err = new Error('graphQL server bad response!')
+        return Promise.reject(err)
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  const handleChangeAuthor = event => {
+    let val = event.target.value
+    setAuthor(val)
+    // reset search by title
+    setSearchTitleInput()
+  }
+
   return (
     <>
-      {called && loading && (
+      {/* search inputs */}
+      <div className='search-area'>
+        <input
+          type='search'
+          placeholder='Search'
+          value={searchTitleInput || ''}
+          onChange={handleSearchByTitle}
+        />
+        <select value={author} onChange={handleChangeAuthor}>
+          <option disabled value=''>Filter by author name</option>
+          {authors && authors.map(author => (
+            <option key={author.id} value={author.id}>
+              {author.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* display items */}
+      {((called && loading) || (calledAuth && loadingAuth)) && (
         <p>Loading...</p>
       )}
-      {!loading && data && (
-        <>
-
-          <Items currentItems={currentItems} />
-        </>
+      {((!loading && !loadingAuth) && (data || dataAuth)) && (
+        <Items currentItems={currentItems} />
       )}
-      <Pagination count={count} page={page} onChange={handleChange} />
+      {((data && data.posts && data.posts.data && data.posts.data.length === 0) ||
+        (dataAuth && dataAuth.user && dataAuth.user.posts && dataAuth.user.posts.data && dataAuth.user.posts.data.length === 0)) && (
+        <p>no results!</p>
+      )}
+      {/* display paginate bar */}
+      {count && count > 1 && (
+        <Pagination count={count} page={page} onChange={handleChange} />
+      )}
     </>
   )
 }
